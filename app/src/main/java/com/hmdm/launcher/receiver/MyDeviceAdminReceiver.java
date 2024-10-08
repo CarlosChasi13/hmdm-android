dba87e7a4040611d534af6bf37048957e6ea3243
CarlosChasi13 <120142318+CarlosChasi13@users.noreply.github.com>
2024-10-08 12:23:32 -0500

    recursos_gráficos

---
+++ app/src/main/java/com/hmdm/launcher/receiver/MyDeviceAdminReceiver.java
@@ -0,0 +1,7 @@
+package com.hmdm.launcher.receiver;
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
+    // Puedes dejar este bloque vacío o agregar métodos personalizados si es necesario.
+}
