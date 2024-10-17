7be237059c9ad370773a10e4fbc45762e136b9e1
CarlosChasi13 <120142318+CarlosChasi13@users.noreply.github.com>
2024-10-17 07:49:03 -0500

    Desactivar botones

--- app/src/main/java/com/hmdm/launcher/receiver/MyDeviceAdminReceiver.java
+++
@@ -1,7 +0,0 @@
-package com.hmdm.launcher.receiver;
-
-import android.app.admin.DeviceAdminReceiver;
-
-public class MyDeviceAdminReceiver extends DeviceAdminReceiver {
-    // Puedes dejar este bloque vacío o agregar métodos personalizados si es necesario.
-}
